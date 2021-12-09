816ad1a277fcdd90a0748d9bd57ea3471a354a55
Henrique Ramos <henriqueramos0106@gmail.com>
2021-12-08 23:47:10 -0300

    Criando temas para o app

--- App.tsx
+++ App.tsx
@@ -1,9 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import AppLoading from 'expo-app-loading';
+import { ThemeProvider } from 'styled-components';
+import {
+  useFonts,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+} from '@expo-google-fonts/poppins';
 import { Dashboard } from './src/screens/Dashboard';
-
+import { theme } from './src/global/styles/theme';
 
 export default function App() {
-  return <Dashboard />
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+  }); 
+
+  if (!fontsLoaded) return <AppLoading />
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <Dashboard />
+    </ThemeProvider>
+  );
 }
 
